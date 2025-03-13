b474007aa31d2b64b9bcb7d2d56644ec1250a4d3
Zero-1929 <142957839+Zero-1929@users.noreply.github.com>
2025-03-12 23:58:15 -0500

    Delete GestionHospital directory

--- GestionHospital/src/main/java/ClonableHistorialMedico.java
+++
@@ -1,6 +0,0 @@
-public interface ClonableHistorialMedico extends Cloneable {
-
-    HistorialMedico clone();
-
-}
-
--- GestionHospital/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module co.edu.uniquindio.poo.gestionhospital {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens co.edu.uniquindio.poo.gestionhospital to javafx.fxml;
-    exports co.edu.uniquindio.poo.gestionhospital;
-}
